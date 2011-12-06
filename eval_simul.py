d7f47749438ce0777fd2a6866eea19c52fb79643
Guillaume Gay <elagachado@gmail.com>
2011-12-06 17:23:15 +0100

    copied first_min and fwhm functions in eval_simul to avoid dependency on glygs_utils

--- eval_simul.py
+++ eval_simul.py
@@ -17,7 +17,7 @@ pyximport.install()
 #from pylab import *
 #
 
-from glygs_utils.array_utils import first_min, fwhm
+#from glygs_utils.array_utils import first_min, fwhm
 
 
 ## local imports
@@ -309,3 +309,36 @@ def max_freqs(KD, show_fig = True):
     return max_fs
 
 
+def fwhm(wave):
+    '''
+    Returns full width at half maximum
+    '''
+    
+    m = wave.max()
+    x = wave.argmax()
+
+    right = left = nan
+    
+    wave /= m
+    for i in range(min(wave[x:-1].size, wave[1:x].size)):
+        if wave[x+i] <= 0.5 < wave[x+i+1]:
+            right = i
+        if wave[x-i] <= 0.5 < wave[x-i-1]:
+            left = i
+
+        if isfinite(right) and isfinite(left):
+            return right + left
+
+
+def first_min(wave):
+    '''
+    returns the first minimum of an array, starting from the first element 
+    if interp = True, evaluates the minimum position via linear interpolation
+    of diff(wave)
+
+    '''
+    
+    for i in range(1,wave[1:-1].size):
+        if wave[i-1] > wave[i] < wave[i+1]:
+            return i
+    return None
